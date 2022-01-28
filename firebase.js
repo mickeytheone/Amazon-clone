77def1e51eb419defc30a75695ac90de29d7ac20
mickeytheone <mickeyfeng15@gmail.com>
2022-01-28 15:31:46 -0600

    added firebases and cart html content

---
+++ firebase.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDA8K4RZC9s4d-6o9ffDXDAXXkEVpvWZ4Q",
+  authDomain: "clone-8d492.firebaseapp.com",
+  projectId: "clone-8d492",
+  storageBucket: "clone-8d492.appspot.com",
+  messagingSenderId: "10345870347",
+  appId: "1:10345870347:web:fbe73adb2b69ae31a9c30e",
+  measurementId: "G-NFQTCD4RCR",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
